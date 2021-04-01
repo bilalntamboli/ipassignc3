08d1183810844ff9d25b78d0e473fdfcb5a69527
Bilal Tamboli <bilaltamboli@protonmail.com>
2021-04-01 08:49:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,42 +18,27 @@
     <div class="main">
       <div class="main_two ">
         <div class="container text-center my-5">
-        <h2>Customer Information</h2>
+           <h2>Customer Information</h2>
         </div>
-     
-           <div class="form">
-               
-              <form method="post" action="products.php">
-                <div class="input">
-                  <input type="text" placeholder="Name" class="input1" name="name" required>
-                </div>
-                <div class="input">
-                  <input type="text" placeholder="Address" class="input1 " name="addr"  required>
-                </div>
-                <div class="input">
-                  <input type="text" placeholder="Phone Number" class="input1"  name="phone"  required>
-                </div>
-                <div class="text-center mt-3">
-                  <button class="btn btn-primary" type="submit"  name="next" >Next</button>
-                </div>
+             <div class="form">
+                  <form method="post" action="products.php">
+                      <div class="input">
+                            <input type="text" placeholder="Name" class="input1" name="name" required>
+                      </div>
+                      <div class="input">
+                           <input type="text" placeholder="Address" class="input1 " name="addr"  required>
+                      </div>
+                      <div class="input">
+                           <input type="text" placeholder="Phone Number" class="input1"  name="phone"  required>
+                      </div>
+                      <div class="text-center mt-3">
+                     <button class="btn btn-primary" type="submit"  name="next" >Next</button>
+                     </div>
               </form>
-            </div>
-          
+            </div>  
         <div>
       </div>
     </div>
-
-
-      
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
-    -->
+      <!-- form Ends -->
   </body>
 </html>
